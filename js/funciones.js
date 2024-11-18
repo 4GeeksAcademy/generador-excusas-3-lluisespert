5a3a94aad8d87df43ae811835b19bb3a9cf9fe1d
Lluis Espert Cuquerella <lluisespertc@gmail.com>
2024-11-18 10:31:40 +0100

    Generador 2 modificado y corregido

--- js/funciones.js
+++ js/funciones.js
@@ -8,13 +8,13 @@ function prueba(){
 
     let cuando = ["Después de la clase", "Mientras estoy durmiendo", "Mientras hago ejercicios", "Durante la comida", "Mientras compro"];
 
-    let x = Math.floor((Math.random() * 3) + 0 );
+    let x = Math.floor((Math.random() * quien.length));
 
-    let x2 = Math.floor((Math.random() * 3) + 0 );
+    let x2 = Math.floor((Math.random() * accion.length));
 
-    let x3 = Math.floor((Math.random() * 2) + 0 );
+    let x3 = Math.floor((Math.random() * porque.length));
 
-    let x4 = Math.floor((Math.random() * 4) + 0 );
+    let x4 = Math.floor((Math.random() * cuando.length));
 
     document.getElementById("excusas").innerHTML = quien[x] + " " + accion[x2] + " " + porque[x3] + " " + cuando[x4];
 
